351c00d9bf851b5e9e8540a94a16f0dced815743
Luban78 <Luban78@gmail.com>
2026-08-12 04:13:25 +0200

    Průhledná systémová navigace Android

--- android/app/src/main/java/cz/luban/notes2go/MainActivity.java
+++ android/app/src/main/java/cz/luban/notes2go/MainActivity.java
@@ -1,5 +1,7 @@
 package cz.luban.notes2go;
 
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 
@@ -13,11 +15,15 @@ public class MainActivity extends BridgeActivity {
 
     getWindow().getDecorView().setSystemUiVisibility(
       View.SYSTEM_UI_FLAG_FULLSCREEN
-        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-        | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
         | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
         | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
         | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
     );
+
+    getWindow().setNavigationBarColor(Color.TRANSPARENT);
+
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+      getWindow().setNavigationBarContrastEnforced(false);
+    }
   }
 }
