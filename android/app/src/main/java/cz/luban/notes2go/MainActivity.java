eaba53e8b5502a524df12e727a0399936eaff775
Luban78 <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>
2026-08-24 12:26:53 +0200

    fix fullscreen MainActivity.java

--- android/app/src/main/java/cz/luban/notes2go/MainActivity.java
+++ android/app/src/main/java/cz/luban/notes2go/MainActivity.java
@@ -16,12 +16,7 @@ public class MainActivity extends BridgeActivity {
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
 
-    getWindow().getDecorView().setSystemUiVisibility(
-      View.SYSTEM_UI_FLAG_FULLSCREEN
-        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-        | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-    );
+    nastavFullscreen();
 
     getWindow().setNavigationBarColor(Color.TRANSPARENT);
 
@@ -44,6 +39,26 @@ public class MainActivity extends BridgeActivity {
         }
       }
     );
+  }
+
+
+  private void nastavFullscreen() {
+    getWindow().getDecorView().setSystemUiVisibility(
+      View.SYSTEM_UI_FLAG_FULLSCREEN
+        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+        | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+    );
+  }
+
+
+  @Override
+  public void onWindowFocusChanged(boolean hasFocus) {
+    super.onWindowFocusChanged(hasFocus);
+
+    if (hasFocus) {
+      nastavFullscreen();
+    }
   }
 
 
